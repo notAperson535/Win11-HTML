a4e20f7e48d9ff715d3beb57b863359ac78bbfd6
notAperson535 <notAperson939@gmail.com>
2022-02-21 18:35:22 -0500

    added search and start menus

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 let startbutton = document.getElementsByClassName("startbutton")[0]
 let startmenu = document.getElementsByClassName("startmenu")[0]
-   
-startbutton.addEventListener("click", ()=>{ console.log("clicked");
-    if(startmenu.style.bottom == "50px"){
-        startmenu.style.bottom = "-655px"
-    }
-    else{
-        startmenu.style.bottom = "50px"
-    }
+
+startbutton.addEventListener("click", ()=>{
+	console.log("clicked");
+	if(startmenu.style.bottom == "50px"){
+		startmenu.style.bottom = "-655px"
+	}
+	else{
+		startmenu.style.bottom = "50px"
+	}
+})
+
+let searchbutton = document.getElementsByClassName("searchbutton")[0]
+let searchmenu = document.getElementsByClassName("searchmenu")[0]
+
+searchbutton.addEventListener("click", ()=>{
+	console.log("clicked");
+	if(searchmenu.style.bottom == "50px"){
+		searchmenu.style.bottom = "-655px"
+	}
+	else{
+		searchmenu.style.bottom = "50px"
+	}
 })

4e1c9e94569560ae18aed5b9cf535b07ca13a577
notAperson535 <95918679+notAperson535@users.noreply.github.com>
2022-02-23 13:43:10 -0500

    fixed transition between start and search menu

--- script.js
+++ script.js
@@ -10,22 +10,36 @@ startbutton.addEventListener("click", ()=>{
 		startmenu.style.bottom = "-655px"
 	}
 	else{
-		startmenu.style.transition = "all 0.3s ease-in"
-		startmenu.style.bottom = "50px"
-		searchmenu.style.bottom = "-655px"
-		searchmenu.style.transition = "all 0.0s ease-in"
+		if(searchmenu.style.bottom == "50px"){
+			startmenu.style.transition = "all 0.0s ease-in"
+			searchmenu.style.bottom = "-655px"
+			startmenu.style.transition = "all 0.0s ease-in"
+			startmenu.style.bottom = "50px"
+		}
+		else{
+			startmenu.style.transition = "all 0.3s ease-in"
+			startmenu.style.bottom = "50px"
+		}
 	}
 })
 
 searchbutton.addEventListener("click", ()=>{
 	console.log("clicked");
 	if(searchmenu.style.bottom == "50px"){
+		searchmenu.style.transition = "all 0.3s ease-in"
 		searchmenu.style.bottom = "-655px"
 	}
 	else{
-		searchmenu.style.transition = "all 0.3s ease-in"
-		searchmenu.style.bottom = "50px"
-		startmenu.style.bottom = "-655px"
-		startmenu.style.transition = "all 0.0s ease-in"
+		if(startmenu.style.bottom == "50px"){
+			startmenu.style.transition = "all 0.0s ease-in"
+			startmenu.style.bottom = "-655px"
+			searchmenu.style.transition = "all 0.0s ease-in"
+			searchmenu.style.bottom = "50px"
+		}
+		else{
+			searchmenu.style.transition = "all 0.3s ease-in"
+			searchmenu.style.bottom = "50px"
+		}
+
 	}
 })

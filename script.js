6892ae5da1dd834cb0c1917e98a7856eaccad834
notAperson535 <notAperson939@gmail.com>
2022-03-01 15:04:36 -0500

    added file explorer entry to start menu and fixed some little things

--- script.js
+++ script.js
@@ -8,27 +8,21 @@ let widgetsmenu = document.getElementsByClassName("widgetsmenu")[0]
 startbutton.addEventListener("click", ()=>{
 	console.log("clicked");
 	if(startmenu.style.bottom == "50px"){
-		startmenu.style.transition = "all 0.3s ease-in"
+		startmenu.style.transition = "all 0.2s ease-in"
 		startmenu.style.bottom = "-655px"
 	}
 	else{
-		if(searchmenu.style.bottom == "50px"){
-			searchmenu.style.transition = "all 0.0s ease-in"
-			searchmenu.style.bottom = "-655px"
-			startmenu.style.transition = "all 0.0s ease-in"
-			startmenu.style.bottom = "50px"
-		}
-		else{
-			startmenu.style.transition = "all 0.3s ease-in"
-			startmenu.style.bottom = "50px"
-		}
+		searchmenu.style.transition = "all 0.0s ease-in"
+		searchmenu.style.bottom = "-655px"
+		startmenu.style.transition = "all 0.2s ease-in"
+		startmenu.style.bottom = "50px"
 	}
 })
 
 searchbutton.addEventListener("click", ()=>{
 	console.log("clicked");
 	if(searchmenu.style.bottom == "50px"){
-		searchmenu.style.transition = "all 0.3s ease-in"
+		searchmenu.style.transition = "all 0.2s ease-in"
 		searchmenu.style.bottom = "-655px"
 	}
 	else{
@@ -39,7 +33,7 @@ searchbutton.addEventListener("click", ()=>{
 			searchmenu.style.bottom = "50px"
 		}
 		else{
-			searchmenu.style.transition = "all 0.3s ease-in"
+			searchmenu.style.transition = "all 0.2s ease-in"
 			searchmenu.style.bottom = "50px"
 		}
 
@@ -49,11 +43,11 @@ searchbutton.addEventListener("click", ()=>{
 widgetsbutton.addEventListener("click", ()=>{
 	console.log("clicked");
 	if(widgetsmenu.style.left == "10px"){
-		widgetsmenu.style.transition = "all 0.3s ease-in"
+		widgetsmenu.style.transition = "all 0.2s ease-in"
 		widgetsmenu.style.left = "-1000px"
 	}
 	else{
-		widgetsmenu.style.transition = "all 0.3s ease-in"
+		widgetsmenu.style.transition = "all 0.2s ease-in"
 		widgetsmenu.style.left = "10px"
 		}
 

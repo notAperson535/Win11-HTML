dc9b6715be66fe9c080edce3b5edcb89c014e12d
notAperson535 <notAperson939@gmail.com>
2022-05-16 17:44:20 -0400

    last fix

--- scripts/iframe-bypass.js
+++ scripts/iframe-bypass.js
@@ -65,7 +65,10 @@ customElements.define('x-frame-bypass', class extends HTMLIFrameElement {
 	}
 	fetchProxy (url, options, i) {
 		const proxy = [
-			'https://api.codetabs.com/v1/proxy/?quest='
+			'https://win11react-proxy.herokuapp.com/',
+			'https://cors.io/?',
+			'https://jsonp.afeld.me/?url=',
+			'https://cors-anywhere.herokuapp.com/'
 		]
 		return fetch(proxy[i] + url, options).then(res => {
 			if (!res.ok)

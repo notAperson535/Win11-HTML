cf2914bb54408bbc7c471e3f4314ffd5c7450827
notAperson535 <notAperson939@gmail.com>
2022-05-27 16:23:13 -0400

    fix calculator link

--- scripts/applist.js
+++ scripts/applist.js
@@ -4,7 +4,7 @@ createapp("word", "apps/word.html", "img/word.ico", "Word");
 createapp("excel", "apps/excel.html", "img/excel.ico", "Excel");
 createapp("powerpoint", "https://app.deckdeckgo.com", "img/powerpoint.ico", "PowerPoint");
 createpremadeapp("notepad", "", "img/notepad.ico", "Notepad");
-createapp("calculator", "apps/calculator.html", "img/calculator.ico", "Calculator");
+createapp("calculator", "https://calculator.notaperson535.is-a.dev", "img/calculator.ico", "Calculator");
 createpremadeappwithtaskbaricon("store", "", "img/store.ico", "Store");
 createapp("vscode", "https://vscode.dev", "img/vscode.ico", "VS Code");
 createapp("solitaire", "https://www.squidbyte.com/games/spidersolitairewindowsxp/", "img/solitaire.ico", "Solitaire");

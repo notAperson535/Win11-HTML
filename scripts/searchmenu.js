6e02b1717c4ec1b9dc13ba29e7e1908d4aaba677
notAperson535 <95918679+notAperson535@users.noreply.github.com>
2022-05-27 10:42:48 -0400

    Add files via upload

---
+++ scripts/searchmenu.js
@@ -0,0 +1,21 @@
+createsearchtopapp("edge", "img/edge.png", "Microsoft Edge");
+createsearchtopapp("notepad", "img/notepad.ico", "Notepad")
+createsearchtopapp("vscode", "img/vscode.ico", "VS Code")
+createsearchtopapp("solitaire", "img/solitaire.ico", "Solitaire")
+createsearchtopapp("store", "img/store.ico", "Microsoft Store")
+
+function createsearchtopapp(appname, icon, visualappname){
+    var topapp = document.createElement("div");
+    document.getElementsByClassName('searchtopapps')[0].appendChild(topapp);
+    topapp.classList.add("searchtopapp");
+
+    var topappicon = document.createElement("img");
+    topapp.appendChild(topappicon);
+    topappicon.src = icon;
+
+    var topappvisualname = document.createElement("p");
+    topapp.appendChild(topappvisualname);
+    topappvisualname.classList.add("searchtopappslabel");
+    topappvisualname.setAttribute("align", "center");
+    topappvisualname.innerHTML = visualappname;
+}
